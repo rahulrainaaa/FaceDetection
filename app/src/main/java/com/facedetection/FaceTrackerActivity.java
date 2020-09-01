bd5cbdec1f088a2a7e4318d085b2a3e559e8bf23
rahul.raina <rahul.raina@ril.com>
2020-09-01 08:09:16 +0530

    Switch to Front cam with 10 FPS.

--- app/src/main/java/com/facedetection/FaceTrackerActivity.java
+++ app/src/main/java/com/facedetection/FaceTrackerActivity.java
@@ -129,8 +129,8 @@ public final class FaceTrackerActivity extends AppCompatActivity {
 
         mCameraSource = new CameraSource.Builder(context, detector)
                 .setRequestedPreviewSize(640, 480)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedFps(30.0f)
+                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                .setRequestedFps(10.0f)
                 .build();
     }
 
